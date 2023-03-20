e9bd499641d55b59cdf94d5df08a35d83803a534
Pedro Botter <pedrobotter2016@gmail.com>
2023-03-20 19:25:00 -0400

    cores desafio1

--- desafios_da_professora/primeiro.py
+++ desafios_da_professora/primeiro.py
@@ -1,11 +1,11 @@
 print("Organizando em ordem crescente")
-num1 = int(input("Primeiro número da lista: "))
-num2 = int(input("Segundo número da lista: "))
-num3 = int(input("Terceito número da lista: "))
+num1 = int(input("\033[1mPrimeiro número da lista: "))
+num2 = int(input("\033[1mSegundo número da lista: "))
+num3 = int(input("\033[1mTerceito número da lista: "))
 if num1 > num2:
     num1, num2 = num2, num1
 if num2 > num3:
     num2, num3 = num3 ,num2
 if num1 > num2:
     num1, num2 = num2, num1
-print(num1, num2, num3)
+print("\033[32m", num1, num2, num3, "\033[0m")
